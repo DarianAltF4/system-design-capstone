93747d3d7d89cf8977f449759db2e5f40e4d98ba
Darian Hogue <hogued96@gmail.com>
2022-06-16 12:36:13 -0400

    repo copy added

---
+++ NoSQL-DB/no-sql-db.js
@@ -0,0 +1,150 @@
+const {Schema, model, mongoose} = require('mongoose');
+const { v4: uuidv4 } = require('uuid');
+
+mongoose.connect('mongodb://localhost:27017/sdc-database')
+  .then(() => {
+    console.log('Connected to SDC Database!');
+  })
+  .catch((err) => {
+    console.log('Connect to DB Error:', err);
+  });
+
+const Review = new Schema({
+  product_id: String,
+  review_id: {type: String || Number, unique: true},
+  summary: String,
+  recommend: Boolean,
+  reported: {type: Boolean, default: false},
+  response: {type: String, default: ''},
+  body: String,
+  date: String,
+  reviewer_name: String,
+  reviewer_email: String,
+  helpfulness: {type: Number, default: 0},
+  photos: [Object],
+  rating: Number,
+  recommend: Boolean,
+  characteristics: {type: Object, default: {}}
+});
+
+const newReview = mongoose.model('newReview', Review);
+
+const etlSaveReview = (data, dataType, review_id) => {
+  if (dataType === 'reviews') {
+    return newReview.insertMany(data);
+  }
+  if (dataType === 'reviewPhotos') {
+    let photo = {url: data.url, id: data.id};
+    return newReview.bulkSave(data);
+  }
+  if (dataType === 'characteristics') {
+    return newReview.bulkWrite(data);
+  }
+};
+
+const getReviews = (params) => {
+
+  let count = params.count || 5;
+  let page = params.page || 1;
+  let product_id = params.product_id;
+  let sort = {};
+  if (params.sort === 'newest') {
+    sort.date = -1;
+  }
+  if (params.sort === 'helpful') {
+    sort.helpfulness = -1;
+  }
+  return newReview.find(
+    {product_id},
+    {
+      _id: false,
+      product_id: false,
+      __v: false,
+      characteristics: false
+    }
+  ).limit(count).sort(sort)
+    .catch((err) => {
+      console.log('hiii')
+      if (err.message === 'Product Does Not Exist') {
+        return 'This product does not exist.';
+      } else {
+        throw err;
+      }
+    });
+};
+
+const getAllReviews = (product_id) => {
+  return newReview.find({product_id}, {
+    _id: false,
+    product_id: false,
+    __v: false
+  })
+    .catch((err) => {
+      throw err;
+    });
+};
+
+const postReview = (params) => {
+  let product_id, rating, summary, body, recommended, name, email, photos, characteristics;
+  ({product_id, rating, summary, body, recommended, name, email, photos, characteristics} = params);
+  let date = Date.now();
+
+  return newReview.find({product_id: `${product_id}`}).limit(-1)
+    .then((prod) => {
+      if (prod.length === 0) {
+        throw new Error('Product Does Not Exist');
+      }
+      let chars = prod[0].characteristics;
+      let charsObj = {};
+      for (let key in chars) {
+        let id = chars[key].id;
+        charsObj[key] = {id: id, value: characteristics[id]};
+      }
+
+      let photosArray = [];
+      for (let i = 0; i < photos.length; i++) {
+        photosArray.push({id: i, url: photos[i]});
+      }
+      return {c: charsObj, p: photosArray};
+    })
+    .then((dataToAdd) => {
+      let allPhotos = dataToAdd.p;
+      let allChars = dataToAdd.c;
+      return newReview.create({
+        review_id: uuidv4(),
+        product_id,
+        rating,
+        summary,
+        body,
+        date,
+        recommend: recommended,
+        reviewer_name: name,
+        reviewer_email: email,
+        photos: allPhotos,
+        characteristics: allChars
+      });
+    })
+    .catch((err) => {
+      console.log(err.message);
+      if (err.message === 'Product Does Not Exist') {
+        return 'This product does not exist.';
+      } else {
+        throw err;
+      }
+
+    });
+};
+
+const helpful = (review_id) => {
+  return newReview.updateOne({review_id}, {$inc: {helpfulness: 1}});
+};
+
+const report = (review_id) => {
+  return newReview.updateOne({review_id}, {reported: true});
+};
+
+const deleteReview = (review_id) => {
+  return newReview.deleteOne({review_id});
+};
+
+module.exports = {newReview, etlSaveReview, getReviews, getAllReviews, postReview, report, deleteReview, helpful};
---
+++ server/server.js
@@ -0,0 +1,155 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const cors = require('cors');
+const port = 8024;
+
+const {getReviews, getAllReviews, postReview, helpful, report, deleteReview} = require('../NoSQL-DB/no-sql-db.js');
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/reviews', (req, res) => {
+  const params = req.query;
+  getReviews(params)
+    .then((results) => {
+      if (results.length === 0) {
+        res.status(404).send('This product does not exist.');
+      } else {
+        let response = {
+          product: params.product_id,
+          page: params.page || 0,
+          count: results.length || 5,
+          results: results
+        };
+        for (let i = 0; i < results.length; i++) {
+          response.results[i].date = new Date(+results[i].date).toISOString();
+        }
+        res.send(response);
+      }
+    })
+    .catch((err) => {
+      console.log('GET Reviews ERROR:', err);
+      res.sendStatus(500);
+    });
+
+});
+
+app.get('/reviews/meta', (req, res) => {
+  const product_id = req.query.product_id;
+  let response = {
+    product_id: product_id,
+    ratings: {},
+    recommended: {},
+    characteristics: {}
+  };
+
+  getAllReviews(product_id)
+    .then((results) => {
+      if (results.length === 0) {
+        res.status(201).send('This product does not exist.');
+      } else {
+        let charsAvg = {};
+        let charIds = {};
+
+        for (let i = 0; i < results.length; i++) {
+          let review = results[i];
+
+          if (response.ratings[review.rating] === undefined) {
+            response.ratings[review.rating] = 1;
+          } else {
+            response.ratings[review.rating] += 1;
+          }
+          if (response.recommended[review.recommend] === undefined) {
+            response.recommended[review.recommend] = 1;
+          } else {
+            response.recommended[review.recommend] += 1;
+          }
+
+          for (let key in review.characteristics) {
+            let value = +review.characteristics[key].value;
+            let id = +review.characteristics[key].id;
+            if (charsAvg[key] === undefined) {
+              charsAvg[key] = value;
+              charIds[key] = id;
+            } else {
+              charsAvg[key] += value;
+            }
+          }
+        }
+
+        for (let key in charsAvg) {
+          charsAvg[key] = {
+            value: charsAvg[key] / results.length,
+            id: charIds[key]
+          };
+        }
+
+        response.characteristics = charsAvg;
+        res.send(response);
+      }
+
+    })
+    .catch((err) => {
+      console.log('GET reviews/meta ERROR:', err);
+      res.sendStatus(500);
+    });
+});
+
+app.post('/reviews', (req, res) => {
+  postReview(req.body)
+    .then((results) => {
+      if (results === 'This product does not exist.') {
+        res.status(201).send('This product does not exist.');
+      } else {
+        res.sendStatus(201);
+      }
+    })
+    .catch((err) => {
+      console.log('POST ERROR:', err);
+      res.sendStatus(500);
+    });
+});
+
+app.put('/reviews/:review_id/helpful', (req, res) => {
+  const id = req.params.review_id;
+  helpful(id)
+    .then(() => {
+      res.sendStatus(204);
+    })
+    .catch((err) => {
+      console.log('PUT helpful ERROR:', err);
+      res.sendStatus(500);
+    });
+});
+
+app.put('/reviews/:review_id/report', (req, res) => {
+  const id = req.params.review_id;
+  report(id)
+    .then(() => {
+      res.sendStatus(204);
+    })
+    .catch((err) => {
+      console.log('PUT report ERROR:', err);
+      res.sendStatus(500);
+    });
+});
+
+app.delete('/reviews/:review_id', (req, res) => {
+  const id = req.params.review_id;
+
+  deleteReview(id)
+    .then(() => {
+      res.sendStatus(204);
+    })
+    .catch((err) => {
+      console.log('DELETE Review ERROR:', err);
+      res.sendStatus(500);
+    });
+});
+
+app.listen(port, () => {
+  console.log(`Server Listening on port ${port}`);
+});
+
+module.exports = app;
